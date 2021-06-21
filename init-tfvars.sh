6705ed1cb697cb9735bf35216c864c2c474d0ff5
Tim Antrobus <tim.antrobus@bjss.com>
2021-06-21 15:25:59 +0100

    Update sed param syntax

--- init-tfvars.sh
+++ init-tfvars.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sed -i '.bak' "s/XXXXXX/$(gcloud organizations list --format='value(ID)')/;s/ZZZZZZ/$(gcloud alpha billing accounts list --format='value(ACCOUNT_ID)' --filter='OPEN=True')/" ${PWD}/terraform.tfvars.example
+sed -i.bak "s/XXXXXX/$(gcloud organizations list --format='value(ID)')/;s/ZZZZZZ/$(gcloud alpha billing accounts list --format='value(ACCOUNT_ID)' --filter='OPEN=True')/" ${PWD}/terraform.tfvars.example
 mv ${PWD}/terraform.tfvars.example ${PWD}/terraform.tfvars.out
