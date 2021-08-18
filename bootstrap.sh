784ff4a730cfcbd97765e23be817b360b4f0913c
Thomas Egwuagu <thomas.egwuagu@bjss.com>
2021-08-18 13:29:58 +0100

    Enhancements to logging and error handling; made file executable

--- bootstrap.sh
+++ bootstrap.sh
@@ -17,36 +17,15 @@ if [ $# -lt 2 ]; then
   exit
 fi
 
-# CONSTANTS FOR EXECUTION STATUSES
+# CONSTANTS FOR EXECUTION STATUSEScd 
 STEP_DONE="DONE!\n"
 STEP_SUCCESS="SUCCESS!\n"
 STEP_FAIL="FAIL!\n"
-
-# CHECKPOINTS ARE EMBEDDED IN STEPS AND INTENDED TO PROMPT USER TO PERFORM CHECKS.
-# YES = ENABLE; NO = DISABLE
-CHECKPOINT_ENABLED="NO"
 
 # REUSABLE FUNCTION TO PREFIX STDOUT MESSAGES/COMMENTS WITH TIMESTAMP
 function timestamp()
 {
     date +"%Y-%m-%d %T"
-}
-
-# REUSABLE FUNCTION TO PAUSE SCRIPT EXECUTION FOR USER CONFIRMATION - DEPENDS ON CHECKPOINT_ENABLED variable 
-function pause()
-{
-if [[ $CHECKPOINT_ENABLED == "YES" ]]; then
-    printf "\n"
-    while true; do
-        read -p "$*" USER_INPUT
-        case $USER_INPUT in 
-            [Yy]* ) break;;
-            [Nn]* ) exit;;
-            * ) echo "Please enter yes or no.";;
-        esac
-    done
-    printf "\n"
-fi
 }
 
 # >>>> STEP 0 - Source conf file and create log file if missing <<<<<<<
@@ -55,7 +34,16 @@ printf "$(timestamp) [0-0]: Sourcing environment variables from .conf file..."
 source $1 # Source config file
 printf $STEP_DONE; sleep 2
 
-printf "$(timestamp) [0-1]: Checking client log file exist ..."
+# CHECKPOINTS ARE EMBEDDED IN STEPS AND INTENDED TO PROMPT USER TO PERFORM CHECKS; YES = ENABLE; NO = DISABLE
+while true; do
+    read -p "$(timestamp) [0-1]: Enable checkpoints? [Y/N]: " CHECKPOINT
+    case $CHECKPOINT in 
+        [Yy]*) CHECKPOINT_ENABLED="Yes"; printf "$(timestamp) [0-2]: Checkpoints ENABLED!\n"; break;;
+        *) CHECKPOINT_ENABLED="No"; printf "$(timestamp) [0-2]: Checkpoints DISABLED!\n"; break;;
+    esac
+done
+
+printf "$(timestamp) [0-3]: Checking client log file exist ..."
 # Check if log directory exists, if not create it
 if [[ ! -d $LOG_DIR ]]; then
     mkdir $LOG_DIR
@@ -65,32 +53,47 @@ if [[ -f $LOG_FILE_PATH ]]; then
     printf "YES!\n"
 else
     printf "NO!\n"
-    printf "$(timestamp) [0-2]: Creating log file..."
+    printf "$(timestamp) [0-4]: Creating log file..."
     touch $LOG_FILE_PATH
     printf "${LOG_FILE_PATH} created successfully!\n"
 fi; sleep 2
 
-# REUSABLE FUNCTION TO CHECK A FILE EXISTS AND REPORTS AN ERROR IF ONE IS NOT FOUND
-function check_file_exists()
+# REDIRECT STDOUT AND STDERR TO LOG FILE. SELECTIVELY DISPLAY STEPS BY REDIRECTING TO >&3
+exec 3>&1 4>&2
+trap 'exec 2>&4 1>&3' 0 1 2 3
+exec 1>>$LOG_FILE_PATH 2>&1
+
+# WRAPPER TO ENABLE SELECTED OUTPUTS TO BOTH CONSOLE AND LOG FILE
+function echothis ()
 {
-    local file="$1"
-    local number="$2"
-    
-    if [ -f $file ]; then
-        printf $STEP_SUCCESS | tee -a $LOG_FILE_PATH
-    else
-        printf $STEP_FAIL | tee -a $LOG_FILE_PATH
-        printf "$(timestamp) [ERROR]: $file file is missing. Execute step #$number.\n" | tee -a $LOG_FILE_PATH
-        exit 1
-    fi
+    printf "$1" | tee -a $LOG_FILE_PATH >&3
+}
+
+# REUSABLE FUNCTION TO PAUSE SCRIPT EXECUTION FOR USER CONFIRMATION - DEPENDS ON CHECKPOINT_ENABLED variable 
+function pause()
+{
+if [[ $CHECKPOINT_ENABLED == "Yes" ]]; then
+    echothis "\n"
+    while true; do
+        #read -p "$*" USER_INPUT
+        echothis "$1"
+        read USER_INPUT
+        case $USER_INPUT in 
+            [Yy]* ) break;;
+            [Nn]* ) exit;;
+            * ) echothis "Please enter yes or no.";;
+        esac
+    done
+    echothis "\n"
+fi
 }
 
 # START OF SWITCH CASE STATEMENTS - INTENDED FOR FOCUSED UNIT TESTING.
 case $2 in
 1) 
 # >>>> STEP 1 - Initialize variables and generate terraform.tfvars <<<<<<<
-    printf "\n>>>>>>>>>> RUNNING: STEP #1.\n\n" | tee -a $LOG_FILE_PATH
-    printf "$(timestamp) [1-01]: Initialising terraform variables..." | tee -a $LOG_FILE_PATH
+    echothis "\n>>>>>>>>>> RUNNING: STEP #1.\n\n"
+    echothis "$(timestamp) [1-1]: Initialising terraform variables..."
     if [[ (-z "${CLIENT_SHORT_NAME}") || 
         (-z "${ORG_ID}") || 
         (-z "${ORG_DOMAIN}") ||       
@@ -100,13 +103,13 @@ case $2 in
         (-z "${WORKLOAD_NETWORK_REGIONS}") 
     ]]
     then
-    printf $STEP_FAIL | tee -a $LOG_FILE_PATH
-    printf "$(timestamp) [ERROR]: One or more variables not populated. Check config file.\n" | tee -a $LOG_FILE_PATH
+    echothis $STEP_FAIL
+    echothis "$(timestamp) [ERROR]: One or more variables not populated. Check config file.\n"
     exit 1
     fi
-    printf $STEP_SUCCESS | tee -a $LOG_FILE_PATH; sleep 1
+    echothis $STEP_SUCCESS; sleep 1
 
-    printf "$(timestamp) [1-02]: Generating terraform.tfvars file..." | tee -a $LOG_FILE_PATH
+    echothis "$(timestamp) [1-2]: Generating terraform.tfvars file..."
 
     cat > ${PWD}/terraform.tfvars <<EOL
     client_short_name = "${CLIENT_SHORT_NAME}"
@@ -118,69 +121,68 @@ case $2 in
     workload_env_subnet_regions = "${WORKLOAD_NETWORK_REGIONS}"
 EOL
 
-    printf $STEP_SUCCESS | tee -a $LOG_FILE_PATH; sleep 1
+    echothis $STEP_SUCCESS; sleep 1
 
-    printf "$(timestamp) [1-03]: Instruction - Review the terraform.tfvar contents before proceeding to the next step:\n\n"
-    cat ${PWD}/terraform.tfvars
-    printf "\n"; sleep 1
+    echothis "$(timestamp) [1-3]: Instruction - Review the terraform.tfvar contents before proceeding to the next step:\n\n"
+    cat ${PWD}/terraform.tfvars | tee -a $LOG_FILE_PATH >&3
+    echothis "\n"; sleep 1
 
     pause "[CHECKPOINT #1]: Do you want to continue? [Y/N]: " # CHECKPOINT!!!!
 
 # >>>> STEP 2 - Initialize terraform, create workspace and plan <<<<<<<
-    printf ">>>>>>>>>> RUNNING: STEP #2. \n\n" | tee -a $LOG_FILE_PATH
+    echothis ">>>>>>>>>> RUNNING: STEP #2. \n\n"
 
-    printf "$(timestamp) [2-1]: Initializing terraform..." | tee -a $LOG_FILE_PATH
-    terraform init 2>&1 >> $LOG_FILE_PATH
-    printf $STEP_SUCCESS | tee -a $LOG_FILE_PATH; sleep 1
+    echothis "$(timestamp) [2-1]: Initializing terraform..."
+    terraform init
+    echothis $STEP_SUCCESS; sleep 1
 
-    printf "$(timestamp) [2-2]: Create new terraform workspace 'bootstrap'..." | tee -a $LOG_FILE_PATH
+    echothis "$(timestamp) [2-2]: Create new terraform workspace 'bootstrap'..."
     
     set +e # Skip warning message about existing workspace
 
-    terraform workspace new bootstrap 1>&2 >> $LOG_FILE_PATH
-    printf $STEP_SUCCESS | tee -a $LOG_FILE_PATH; sleep 1
+    terraform workspace new bootstrap
+    echothis $STEP_SUCCESS; sleep 1
 
     set -e # Enable error reporting
 
-    printf "$(timestamp) [2-3]: Format terraform files and validate..." | tee -a $LOG_FILE_PATH
-    terraform fmt 2>&1 >> $LOG_FILE_PATH
-    terraform validate 1>&2 >> $LOG_FILE_PATH
-    printf $STEP_SUCCESS | tee -a $LOG_FILE_PATH; sleep 1
+    echothis "$(timestamp) [2-3]: Format terraform files and validate..."
+    terraform fmt && terraform validate
+    echothis $STEP_SUCCESS; sleep 1
 
-    printf "$(timestamp) [2-4]: Generate terraform plan..." | tee -a $LOG_FILE_PATH
+    echothis "$(timestamp) [2-4]: Generate terraform plan..."
     if [ -f "${PWD}/$TF_PLAN" ]
     then
-        printf "Found existing plan! Replacing..." | tee -a $LOG_FILE_PATH
+        echothis "Found existing plan! Replacing..."
         rm $TF_PLAN
     fi
     
-    terraform plan -out=$TF_PLAN 2>&1 >> $LOG_FILE_PATH
-    printf $STEP_SUCCESS | tee -a $LOG_FILE_PATH; sleep 1
+    terraform plan -out=$TF_PLAN
+    echothis $STEP_SUCCESS; sleep 1
 
-    printf "$(timestamp) [2-5]: Instruction - Review the generated plan before deploying. Run 'terraform show bootstrap.tfplan'.\n"; sleep 2
+    echothis "$(timestamp) [2-5]: Instruction - Review the generated plan before deploying. Run 'terraform show bootstrap.tfplan'.\n"; sleep 2
 
     pause "[CHECKPOINT #2]: Do you want to continue? [Y/N]: " # CHECKPOINT!!!!
 
 # >>>> STEP 3 - Deploy bootstrap resources <<<<<<<
-    printf "\n>>>>>>>>>> RUNNING: STEP #3.\n\n" | tee -a $LOG_FILE_PATH
-
-    printf "$(timestamp) [3-1]: Deploying the bootstrap infrastructure..." | tee -a $LOG_FILE_PATH
-    terraform apply -auto-approve $TF_PLAN  2>&1 >> $LOG_FILE_PATH
-    printf $STEP_SUCCESS | tee -a $LOG_FILE_PATH; sleep 1
-
-    printf "$(timestamp) [3-2]: Instruction - Review terraform output by running 'terraform show output'.\n"; sleep 2
-
+    echothis "\n>>>>>>>>>> RUNNING: STEP #3.\n\n"
+    echothis "$(timestamp) [3-1]: Deploying the bootstrap infrastructure..."
+    
+    terraform apply -auto-approve $TF_PLAN
+    
+    echothis $STEP_SUCCESS; sleep 1
+    echothis "$(timestamp) [3-2]: Instruction - Review terraform output by running 'terraform show output'.\n"; sleep 2
     pause "[CHECKPOINT #3]: Do you want to continue? [Y/N]: " # CHECKPOINT!!!!
 
 # >>>> STEP 4 - Update and migrate terraform state to GCS backend <<<<
-    printf "\n>>>>>>>>>> RUNNING: STEP #4.\n\n" | tee -a $LOG_FILE_PATH    
-    printf "$(timestamp) [4-1]: Updating terraform gcs backend conf with state bucket name..." | tee -a $LOG_FILE_PATH
+    echothis "\n>>>>>>>>>> RUNNING: STEP #4.\n\n"    
+    echothis "$(timestamp) [4-1]: Updating terraform gcs backend conf with state bucket name..."
 
     export BUCKET_NAME=$(terraform output -raw tf-state-bucket-name)
+    #export BUCKET_NAME="appdev-320214-terraform"
     
     if [ -z "${BUCKET_NAME}" ] # Check variable is populated with bucket name from terraform output
     then
-        printf "$(timestamp) [ERROR]: GCS bucket name not populated.\n" | tee -a $LOG_FILE_PATH
+        echothis "$(timestamp) [ERROR]: GCS bucket name not populated.\n"
         exit 1
     fi
 
@@ -190,37 +192,38 @@ EOL
     mv $WORKDIR/backend.tf.stage $WORKDIR/backend.tf
 
     if [ -f "$WORKDIR/backend.tf" ]; then
-        printf $STEP_SUCCESS | tee -a $LOG_FILE_PATH; sleep 1
+        echothis $STEP_SUCCESS; sleep 1
     else
-        printf "$(timestamp) [ERROR]: Backend.tf file not generated successfully.\n" | tee -a $LOG_FILE_PATH
+        echothis "$(timestamp) [ERROR]: Backend.tf file not generated successfully.\n"
         exit 1
     fi
 
-    printf "$(timestamp) [4-2]: Instruction - Review backend config file below before proceeding to the next step:\n\n"
-    cat ${PWD}/backend.tf
-    printf "\n"; sleep 1
-
+    echothis "$(timestamp) [4-2]: Instruction - Review backend config file below before proceeding to the next step:\n\n"
+    
+    cat ${PWD}/backend.tf | tee -a $LOG_FILE_PATH >&3
+    
+    echothis "\n"; sleep 1
     pause "[CHECKPOINT #4]: Do you want to continue? [Y/N]: " # CHECKPOINT!!!!
 
-    printf "$(timestamp) [4-3]: Migrating terraform state from local to GCS remote..."
-    terraform init -migrate-state -force-copy 1>&2 | tee -a $LOG_FILE_PATH
-    printf $STEP_SUCCESS | tee -a $LOG_FILE_PATH; sleep 1
+    echothis "$(timestamp) [4-3]: Migrating terraform state from local to GCS remote..."
+    terraform init -migrate-state -force-copy | tee -a $LOG_FILE_PATH >&3
+    echothis $STEP_SUCCESS; sleep 1
 
 # >>>> STEP 5 - Generate github keys and configure git global <<<<
-    printf "\n>>>>>>>>>> RUNNING: STEP #5.\n\n" | tee -a $LOG_FILE_PATH
+    echothis "\n>>>>>>>>>> RUNNING: STEP #5.\n\n"
 
-    printf "$(timestamp) [5-1]: Generate keys to connect to private Github repo..." | tee -a $LOG_FILE_PATH
-    ssh-keygen -t rsa -b 4096 -N "" -q -C "${GITHUB_BOT_USER}" -f ~/.ssh/id_github > /dev/null # command output is suppressed
-    ssh-keyscan -t rsa github.com 2>&1 | tee ~/.ssh/known_hosts > /dev/null
+    echothis "$(timestamp) [5-1]: Generate keys to connect to private Github repo..."
+    ssh-keygen -t rsa -b 4096 -N "" -q -C "${GITHUB_BOT_USER}" -f ~/.ssh/id_github 2>&1> /dev/null # suppress output
+    ssh-keyscan -t rsa github.com 2>&1 | tee ~/.ssh/known_hosts > /dev/null 2&>1 # suppress output
     cat ssh_config_template > ~/.ssh/config
-    printf $STEP_SUCCESS | tee -a $LOG_FILE_PATH; sleep 1
+    echothis $STEP_SUCCESS; sleep 1
 
-    printf "$(timestamp) [5-2]: Configure the cloudshell git session (email, name and credential)..." | tee -a $LOG_FILE_PATH
-    git config --global user.email "${GITHUB_BOT_USER}" && git config --global user.name "${GITHUB_BOT_NAME}" 2>&1 >> $LOG_FILE_PATH
-    git config --global credential.https://source.developers.google.com.helper gcloud.sh 2>&1 >> $LOG_FILE_PATH
-    printf $STEP_SUCCESS | tee -a $LOG_FILE_PATH; sleep 1
+    echothis "$(timestamp) [5-2]: Configure the cloudshell git session (email, name and credential)..."
+    git config --global user.email "${GITHUB_BOT_USER}" && git config --global user.name "${GITHUB_BOT_NAME}"
+    git config --global credential.https://source.developers.google.com.helper gcloud.sh
+    echothis $STEP_SUCCESS; sleep 1
 
-    printf "$(timestamp) [5-3]: InstructionS -
+    echothis "$(timestamp) [5-3]: Instructions -
     Add the public SSH key as a deploy key on the Github repo.
     Copy the generated public key and add it as a deploy key on the private Github repo.
     See instructions here : $DEPLOY_KEY_DOC_LINK
@@ -231,34 +234,34 @@ EOL
 
 6) # Retained case statement for focused testing and will be removed following successful runs.
 # >>>> STEP 6 - Clone the private GitHub repo and push into cloud source repo <<<<
-    printf "\n>>>>>>>>>> RUNNING: STEP #6.\n\n" | tee -a $LOG_FILE_PATH
+    echothis "\n>>>>>>>>>> RUNNING: STEP #6.\n\n"
 
-    printf "$(timestamp) [6-1]: Exporting env variables..." | tee -a $LOG_FILE_PATH 
+    echothis "$(timestamp) [6-1]: Exporting env variables..." 
     export GITHUB_SSH_URL=$(echo ${GITHUB_URL} | sed 's/https:\/\/github.com\//git\@github.com:/;s/$/.git/')
     export SEED_PROJ=$(terraform output -raw seed_project_id)
     export GITHUB_REPO_NAME=$(basename ${GITHUB_URL})
-    printf $STEP_SUCCESS | tee -a $LOG_FILE_PATH
+    echothis $STEP_SUCCESS
 
-    printf "$(timestamp) [6-2]: Cloning private GitHub repo..." | tee -a $LOG_FILE_PATH 
-    cd ${HOME} && git clone ${GITHUB_SSH_URL} 2>&1 >> $LOG_FILE_PATH
-    printf $STEP_SUCCESS | tee -a $LOG_FILE_PATH; sleep 1
+    echothis "$(timestamp) [6-2]: Cloning private GitHub repo..." 
+    cd ${HOME} && git clone ${GITHUB_SSH_URL}
+    echothis $STEP_SUCCESS; sleep 1
 
-    printf "$(timestamp) [6-3]: Add remote origin, checkout and push into cloud source repo..." | tee -a $LOG_FILE_PATH  
+    echothis "$(timestamp) [6-3]: Add remote origin, checkout and push into cloud source repo..."  
     cd ${HOME}/${GITHUB_REPO_NAME}
-    git remote add google https://source.developers.google.com/p/${SEED_PROJ}/r/${GITHUB_REPO_NAME} 2>&1 >> $LOG_FILE_PATH
-    git push --all google 2>&1 >> $LOG_FILE_PATH
-    git checkout --track remotes/origin/develop && git push google 2>&1 >> $LOG_FILE_PATH
-    printf $STEP_SUCCESS | tee -a $LOG_FILE_PATH; sleep 2
+    git remote add google https://source.developers.google.com/p/${SEED_PROJ}/r/${GITHUB_REPO_NAME}
+    git push --all google
+    git checkout --track remotes/origin/develop && git push google
+    echothis $STEP_SUCCESS; sleep 2
 ;;
 
 7) # Retained case statement for focused testing and will be removed following successful runs.
 # >>>> STEP 7 - Run terraform to add cloud build CI job triggers for next LZ phase <<<<
-    printf "\n>>>>>>>>>> RUNNING: STEP #7.\n\n" | tee -a $LOG_FILE_PATH
-    printf "$(timestamp) [7-1]: invoke terraform to provision the LZ cloud build job triggers..." | tee -a $LOG_FILE_PATH  
-    cd ${WORKDIR} && terraform apply -auto-approve -var="enable_cb_triggers=true" 2>&1 >> $LOG_FILE_PATH
-    printf $STEP_SUCCESS | tee -a $LOG_FILE_PATH; sleep 2
+    echothis "\n>>>>>>>>>> RUNNING: STEP #7.\n\n"
+    echothis "$(timestamp) [7-1]: invoke terraform to provision the LZ cloud build job triggers..."  
+    cd ${WORKDIR} && terraform apply -auto-approve -var="enable_cb_triggers=true"
+    echothis $STEP_SUCCESS; sleep 2
 
-    printf "\n\n Bootstrap phase for $CLIENT_SHORT_NAME landing zone completed successfull!!!!\n" | tee -a $LOG_FILE_PATH
+    echothis "\n\n Bootstrap phase for $CLIENT_SHORT_NAME landing zone completed successfull!!!!\n"
 ;;
 
 esac
